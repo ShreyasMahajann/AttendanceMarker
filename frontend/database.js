efb62b5ed45fbe69afbd5840a36c38a35c652df0
Shreyas Mahajan <shreyasmahajan05@gmail.com>
2025-01-30 07:12:50 +0530

    env

--- frontend/database.js
+++ frontend/database.js
@@ -1,6 +1,7 @@
 "use server";
 
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
 let userSchema = new mongoose.Schema({
     username:String,
@@ -46,7 +47,8 @@ const connectDB = async () => {
     if (mongoose.connections[0].readyState) {
       return;
     }
-    await mongoose.connect('mongodb://127.0.0.1:27017/user');
+    dotenv.config();
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   };
   
